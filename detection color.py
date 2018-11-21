90702fda9bbe4f7d921bc62e200c2426498c9b55
Pitchoun <pierrecaprassesjb@gmail.com>
2018-11-21 11:17:24 +0100

    premier code virtual mousegit status

--- detection color.py
+++ detection color.py
@@ -8,6 +8,7 @@ cam= cv2.VideoCapture(0)
 kernelOpen=np.ones((5,5))
 kernelClose=np.ones((20,20))
 
+
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 while True:
@@ -23,17 +24,22 @@ while True:
     maskClose=cv2.morphologyEx(maskOpen,cv2.MORPH_CLOSE,kernelClose)
 
     maskFinal=maskClose
+
     _, conts, _=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
-    cv2.drawContours(img,conts,-1,(255,0,0),3)
+    #cv2.drawContours(img,conts,-1,(255,0,0),3)
+
+
     for i in range(len(conts)):
         x,y,w,h=cv2.boundingRect(conts[i])
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255), 2)
         #cv2.putText(img,, str(i+1),,font,(0,255,255))
         cv2.putText(img,str(i+1),(x,(y+h)), font, 2,(0,255,255),1,cv2.LINE_AA)
+        cv2.circle(img, (x,y),2,(0,0,255),2)
     cv2.imshow("maskClose",maskClose)
     cv2.imshow("maskOpen",maskOpen)
     cv2.imshow("mask",mask)
     cv2.imshow("cam",img)
-    cv2.waitKey(1)
-    #cv2.destroyAllWindows()
+    if cv2.waitKey(1) == 27:
+        cv2.destroyAllWindows()
+        break
